a72dc280bcffe3e1d3f068fb4cc61fe27581673d
MARS <leprcoin@gmail.com>
2023-01-22 23:29:51 +0400

    ggfd

---
+++ Exam010_Met/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = {1,23,345,75,79,7890,9};
+int n = array.Length;
+int find = 75;
+int index = 0;
+
+while (index < n)
+{
+    if (array [index] == find)
+    {
+        Console.WriteLine (index);
+        break;
+    }
+    index++;
+}
